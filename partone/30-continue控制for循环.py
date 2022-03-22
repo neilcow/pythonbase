f85878d75be980178423127089aed04a52abbee7
niujinliang <2415793@qq.com>
2022-03-23 00:05:12 +0800

    day06

---
+++ partone/30-continue控制for循环.py
@@ -0,0 +1,5 @@
+str1 = 'test'
+for i in str1:
+    if i == 'e':
+        continue
+    print(i)
---
+++ partone/31-while-else.py
@@ -0,0 +1,6 @@
+i = 1
+while i < 5:
+    print('这是测试while')
+    i += 1
+else:
+    print('循环结束后才执行的')
