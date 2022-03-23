7a3963b1bc1b15272e67920a073195534a85b6a6
niujinliang <2415793@qq.com>
2022-03-23 23:06:53 +0800

    day09

--- partone/34-for...else.py
+++ partone/34-for...else.py
@@ -1,7 +1,8 @@
 str1 = 'test'
 for i in str1:
     if i == 's':
-        break
+        # break
+        continue
     print(i)
 else:
     print('for循环结束了')
---
+++ parttwo/01-认识字符串.py
@@ -0,0 +1,15 @@
+name1 = 'test'
+print(name1)
+print(type(name1))
+
+name2 = "test2"
+print(name2)
+print(name2)
+print(type(name2))
+
+# 三引号
+name3 = ''' test3
+          sdf '''
+print(type(name3))
+name4 = """ test4 """
+print(type(name4))
