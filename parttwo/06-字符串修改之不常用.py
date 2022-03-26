9f1ceba8c437e1db9881c83c5529699d77dabca8
niujinliang <2415793@qq.com>
2022-03-26 14:39:40 +0800

    day10

---
+++ parttwo/06-字符串修改之不常用.py
@@ -0,0 +1,21 @@
+# capitalize()
+mystr = '  hello World and python and python  '
+str = mystr.capitalize()
+print(str)
+# 输出 Hello world and python and python
+str1 = mystr.title()
+print(str1)
+# Hello World And Python And Python
+str2 = mystr.lower()
+print(str2)
+str3 = mystr.upper()
+print(str3)
+
+str4 = mystr.lstrip()
+print(str4)
+
+str5 = mystr.rstrip()
+print(str5)
+
+str6 = mystr.strip()
+print(str6)
---
+++ parttwo/07-字符串判断.py
@@ -0,0 +1,15 @@
+# 检查字符串是否以指定子串开头
+mystr = 'hello world'
+# print(mystr.startswith('hel', 0, 20))
+#
+# print(mystr.endswith('rld', 0, 50))
+
+mystr1 = '3242341'
+# print(mystr1.isalpha())
+# print(mystr1.isdigit())
+
+mystr2 = '1233sad'
+# print(mystr2.isalnum())
+
+mystr3 = ' '
+print(mystr3.isspace())
